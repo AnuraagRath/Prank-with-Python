75dcadb099f04698fa0b00c8ea3a3a4c600cfec1
Anuraag Rath <72731854+AnuraagRath@users.noreply.github.com>
2020-10-12 18:08:31 +0530

    Update prank.py

--- prank.py
+++ prank.py
@@ -2,5 +2,5 @@ import webbrowser
 import time
 
 while True:
-    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    webbrowser.open('https://r4---sn-h5576nee.googlevideo.com/videoplayback?expire=1602525885&ei=XUaEX--uMtXKvASKiYboBw&ip=43.229.88.204&id=o-AL_LFqqQdyrfNSmcDwQG2mE7F3ZNouJsQ3NupBkLqwM-&itag=18&source=youtube&requiressl=yes&gcr=in&vprv=1&mime=video%2Fmp4&gir=yes&clen=15929856&ratebypass=yes&dur=212.091&lmt=1578933304205652&fvip=4&fexp=9466588,23915654&beids=9466588&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhANxAG7udHozFv6oHYVnmcN6Gs-jxiO5b55uTjhsfxZ4RAiAMjvJuMMKBZf3_kWIeVWSjO2kNokOrdjn1K9msbaI_lA%3D%3D&video_id=dQw4w9WgXcQ&title=Rick+Astley+-+Never+Gonna+Give+You+Up+%28Video%29&redirect_counter=1&rm=sn-3jja-jv3s7l&req_id=57d9490de41ba3ee&cms_redirect=yes&ipbypass=yes&mh=7c&mm=29&mn=sn-h5576nee&ms=rdu&mt=1602506088&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgSRqLHSLhBCEitfzdSNPn-ytmDIgTUZWENN_JwpuoOLoCIQCFjiJb0Gctk6KNtN-DAzSTli3fqhOyFiobQZ8l7LWjWA%3D%3D')
     time.sleep(2)
